7021e329a7dbce9ba0bbddc41b732ee61e364d52
meysamhadigheh <hadigheh.meysam@gmail.com>
2020-12-29 17:45:13 +0330

    Initial commit

---
+++ app/src/main/java/info/meysam/coverto/ui/activities/MainActivity.kt
@@ -0,0 +1,14 @@
+package info.meysam.coverto.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import info.meysam.coverto.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
