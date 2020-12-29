0260b8106bec157a03153b4468cbbdcfb8c2b50b
meysamhadigheh <hadigheh.meysam@gmail.com>
2020-12-29 19:02:01 +0330

    add SplashActivity.kt

---
+++ app/src/main/java/info/meysam/coverto/ui/activities/SplashActivity.kt
@@ -0,0 +1,27 @@
+package info.meysam.coverto.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import info.meysam.coverto.R
+
+class SplashActivity : BaseActivity() {
+
+    override fun setContentViewActivity() {
+
+        setContentView(R.layout.activity_splash)
+
+    }
+
+
+    override fun getIntentData() {
+    }
+
+    override fun instantiateViews() {
+    }
+
+    override fun setViewListeners() {
+    }
+
+    override fun setActivityContent() {
+    }
+}
