cf480fab1433e5bb728bbb16d967f29470768a77
meysamhadigheh <hadigheh.meysam@gmail.com>
2020-12-29 19:28:50 +0330

    add animation to SplashActivity.kt

--- app/src/main/java/info/meysam/coverto/ui/activities/SplashActivity.kt
+++ app/src/main/java/info/meysam/coverto/ui/activities/SplashActivity.kt
@@ -1,8 +1,10 @@
 package info.meysam.coverto.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import info.meysam.coverto.R
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : BaseActivity() {
 
@@ -17,11 +19,25 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun instantiateViews() {
+
+        animation.setAnimation(R.raw.lf20_dy2leguq)
+
+
     }
 
     override fun setViewListeners() {
     }
 
     override fun setActivityContent() {
+
+        val handler = android.os.Handler()
+
+        handler.postDelayed({
+
+            startActivity(Intent(context, MainActivity::class.java))
+            finish()
+
+        }, 3500)
+
     }
 }
